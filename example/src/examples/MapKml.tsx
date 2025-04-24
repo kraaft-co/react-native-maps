7954261055f72688a9436b5233e5d5ce586fd441
Alexis CHAPPRON <bullrot-10@hotmail.fr>
2025-04-24 14:34:41 +0200

    add multiple kml example

--- example/src/examples/MapKml.tsx
+++ example/src/examples/MapKml.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import {StyleSheet, View, Dimensions, ScrollView, Button} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import React, {useState, useRef, useMemo} from 'react';
+import { StyleSheet, View, Dimensions, ScrollView, Button } from 'react-native';
+import MapView, {MapViewProps, Marker} from 'react-native-maps';
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 const LATITUDE = 46.2276;
 const LONGITUDE = 2.2137;
@@ -14,53 +14,64 @@ const kmlFiles = [
   'https://pastebin.com/raw/qwZn8dRU',
 ];
 
-export default class MapKml extends React.Component<any, any> {
-  map: any;
-  constructor(props: any) {
-    super(props);
+type MapKmlProps = Pick<MapViewProps, "provider">
 
-    this.state = {
-      selectedKml: kmlFiles[0],
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LATITUDE_DELTA,
-      },
-    };
-  }
+const MapKml = ({ provider }: MapKmlProps) => {
+  const mapRef = useRef(null);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          ref={ref => {
-            this.map = ref;
-          }}
-          provider={this.props.provider}
-          style={styles.map}
-          initialRegion={this.state.region}
-          kmlSrc={this.state.selectedKml}>
-           <Marker
-            coordinate={this.state.region}
-            title="Test"
-            description="Test"
-          />
-        </MapView>
-        <View style={styles.simpleKmlSelectionSheet}>
-          <ScrollView>
-            {kmlFiles.map((kmlFile, index) => (
-              <Button
-                key={kmlFile} title={`file ${index} ${this.state.selectedKml === kmlFile ? '(selected)' : ''}`}
-                onPress={() => this.setState({selectedKml: kmlFile})}
-              />
-            ))}
-          </ScrollView>
-        </View>
+  const [selectedKmls, setSelectedKmls] = useState(new Set<string>());
+
+  const selectedKmlStr = useMemo<Array<string>>(()=> {
+    return [...selectedKmls.values()];
+  }, [selectedKmls]);
+
+  const [region] = useState({
+    latitude: LATITUDE,
+    longitude: LONGITUDE,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LATITUDE_DELTA,
+  });
+
+  const handleKmlSelection = (kmlFile: string) => {
+    setSelectedKmls(oldSet => {
+      if (oldSet.has(kmlFile)) {
+        oldSet.delete(kmlFile);
+      }else{
+        oldSet.add(kmlFile);
+      }
+        return new Set([...oldSet]);
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        ref={mapRef}
+        provider={provider}
+        style={styles.map}
+        initialRegion={region}
+        kmlSrc={selectedKmlStr}
+      >
+        <Marker
+          coordinate={region}
+          title="Test"
+          description="Test"
+        />
+      </MapView>
+      <View style={styles.simpleKmlSelectionSheet}>
+        <ScrollView>
+          {kmlFiles.map((kmlFile, index) => (
+            <Button
+              key={kmlFile}
+              title={`file ${index} ${selectedKmls.has(kmlFile) ? '(selected)' : ''}`}
+              onPress={() => handleKmlSelection(kmlFile)}
+            />
+          ))}
+        </ScrollView>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -76,6 +87,8 @@ const styles = StyleSheet.create({
     height,
   },
   simpleKmlSelectionSheet: {
-    height: "20%",
+    height: '20%',
   },
 });
+
+export default MapKml;
